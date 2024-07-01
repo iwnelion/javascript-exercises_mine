3b8905805f09e6b9a5353ec1b5f1fd4014b87e90
iwnelion <anname17@hotmail.com>
2024-07-01 19:19:02 +0300

    solved 07 js

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+  let cel=(f-32)*5/9;
+  let rCel=Math.round(cel*10)/10;
+  return rCel;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+  let far=c*9/5+32;
+  let rFar=Math.round(far*10)/10;
+  return rFar;
 };
 
 // Do not edit below this line
