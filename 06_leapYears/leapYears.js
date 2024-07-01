24086e32426fda60fc31383a97c70905ef7c324f
iwnelion <anname17@hotmail.com>
2024-07-01 19:00:12 +0300

    solved 06 js

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,14 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    const isDivBy4=year%4===0;
+    const isCentury=year%100===0;
+    const isDivBy400=year%400===0;
 
-};
+    if(isDivBy4 && (!isCentury || isDivBy400)){
+            return true;
+        }else{
+            return false;
+        }
+    };
 
 // Do not edit below this line
 module.exports = leapYears;
