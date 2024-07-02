46ac7fa167fa2041f7a0443c0b4f5c2cc12dc93a
iwnelion <anname17@hotmail.com>
2024-07-02 20:08:38 +0300

    solved 10 js

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,24 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    let count;
+    if(typeof num!=='number'){
+        count=parseInt(num)
+    }else{
+        count=num;
+    }
+    if(count<0){
+        return 'OOPS';
+    }
+    if(count===0){
+        return 0;
+    }
+    let first=1;
+    let sec=0;
+    for(let i=2;i<=count;i++){
+        let temp=first+sec;
+        sec=first;
+        first=temp;
+    }
+    return first;
 };
 
 // Do not edit below this line
