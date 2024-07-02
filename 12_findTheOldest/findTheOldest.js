1d2937d27e6ad993e18b668c8d42bb4cf8ed956c
iwnelion <anname17@hotmail.com>
2024-07-02 20:34:07 +0300

    solved 12 js

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
-
+const getAge=function(birth, death){
+    if(!death){
+        death=new Date().getFullYear();
+    }
+    return death-birth;
+}
+const findTheOldest = function(ppl) {
+    return ppl.reduce((oldest, currentP)=>{
+        const oldestAge=getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        const currentAge=getAge(
+            currentP.yearOfBirth,
+            currentP.yearOfDeath
+        );
+        return oldestAge<currentAge ? currentP : oldest;
+    });
 };
 
 // Do not edit below this line
