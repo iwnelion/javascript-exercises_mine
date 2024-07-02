5dc510fedc194d693062c37ee9d37b75fb9e8429
iwnelion <anname17@hotmail.com>
2024-07-02 19:45:15 +0300

    solved 09 js

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
+    const cleanString=word
+    .toLowerCase()
+    .split('')
+    .filter((character)=>alphanumerical.includes(character))
+    .join('');
 
+    const reversedStr=cleanString.split('').reverse().join("");
+
+    return cleanString===reversedStr;
 };
 
 // Do not edit below this line
